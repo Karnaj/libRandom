61323bbf8c95268b0463bc92d2a6aec229d96de2
Karnaj <yoan.geran@hotmail.fr>
2018-01-20 02:58:36 +0100

    Create utils.h

---
+++ utils.h
@@ -0,0 +1,9 @@
+#ifndef UTILS_H_INCLUDED
+#define UTILS_H_INCLUDED
+
+double rand_fun(void);
+int randint(int a, int b);
+void swap(int *a, int *b);
+void shuffle_int(int ary[], int size);
+
+#endif
